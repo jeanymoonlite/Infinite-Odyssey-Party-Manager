66e63ba778ef6eae21f15f3457b17a0d23ba061e
jeanymoonlite <Jeany.moonlite@gmail.com>
2023-06-03 16:24:21 -0400

    Listed every command in a txt file
    * Listed every command the program will have along with their descriptions

--- Party Manager/Party Manager/src/controller/command/Help.java
+++ Party Manager/Party Manager/src/controller/command/Help.java
@@ -1,5 +1,22 @@
 package controller.command;
 
-public class Help {
+import model.Manager;
+import view.IOManagerTextView;
 
+public final class Help extends ACommand {
+
+  /**
+   * Constructs a new {@code Help}.
+   *
+   * @param model the model to use
+   * @param view  the view to use to render messages
+   */
+  public Help(Manager model, IOManagerTextView view) {
+    super(model, view);
+  }
+
+  @Override
+  public void run() {
+
+  }
 }
